3066237edb4b7538439c8a5b74d7adb000e7c732
Jiahui0818 <chenjiahui.bit@gmail.com>
2014-03-19 01:08:21 +0100

    small changes

--- js/panel.js
+++ js/panel.js
@@ -33,13 +33,16 @@ graypanel.selectAll("text").remove();
 			.attr("class", "names")
 			.attr("width", Width)
 			.text(name);
+			
 		displayCrimenumbers(totalcrimes);
 	}
 
 }
 
-function displayCrimenumbers(numbers){
+function displayCrimenumbers(numbers){	
 	
+//var Px = d3.scale.linear().range([0, Width]).domain(0, 19078624);
+
 var total = d3.select("#totalcrimes");
 
 total.selectAll("p").remove();
@@ -48,6 +51,20 @@ total.append("p")
 			.attr("class", "number")
 			.attr("width", Width)
 			.text(numbers);
+
+total.selectAll("rect").remove();
+
+/*total.append("rect")
+	.attr("x", 10)
+	.attr("y", 0)
+	.attr("id", "bars")
+	.attr("class", "bar")
+	.attr("width", 19078624/numbers)
+	.attr("height", 10)
+	.attr("transform", "translate( " + 65  + ", 40)")
+	.text(numbers);
+	
 			
-console.log("crimenumbers" + numbers);
+console.log("crimenumbers" + numbers);*/
+
 }
