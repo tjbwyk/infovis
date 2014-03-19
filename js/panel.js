23daf71e1148a8d30dfbb5ad01106fd41ece20be
Jiahui0818 <chenjiahui.bit@gmail.com>
2014-03-19 01:39:49 +0100

    no message

--- js/panel.js
+++ js/panel.js
@@ -1,13 +1,12 @@
 var Width = 160, Height = 45;
 
+var id = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,45];
 
-displayDistName(TCrimes, []);
+displayDistName(TCrimes, id);
 
 function displayDistName(TCrimes, id){
 
 var name = [];
-
-var id = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,45];
 
 var graypanel = d3.select("#distNames");  
  
